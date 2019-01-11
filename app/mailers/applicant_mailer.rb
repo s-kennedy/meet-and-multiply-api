9d4c5b340f4d66b231a7962de22dd2917ecef292
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-01-10 20:42:43 -0500

    WIP update automated email templates

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -3,6 +3,13 @@ class ApplicantMailer < ApplicationMailer
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::UrlHelper
 
+  ORIGINATOR_PAGE_LOOKUP = {
+    "Originator 1" => "https://www.meetandmultiply.org/",
+    "Originator 2" => "https://www.meetandmultiply.org/",
+    "Originator 3" => "https://www.meetandmultiply.org/",
+    "Originator 4" => "https://www.meetandmultiply.org/",
+  }
+
   def confirm_email(applicant)
     @applicant = applicant
     mail(to: @applicant.email, subject: 'We have received your application')
@@ -11,12 +18,14 @@ class ApplicantMailer < ApplicationMailer
   def acceptance_email(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
+    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
     mail(to: @applicant.email, subject: "You’ve been accepted to attend Meet & Multiply")
   end
 
   def more_info(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
+    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
     mail(to: @applicant.email, subject: "More information needed for your Meet & Multiply application")
   end
 
@@ -28,6 +37,7 @@ class ApplicantMailer < ApplicationMailer
   def reminder(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
+    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
     mail(to: @applicant.email, subject: "Please confirm your attendance at Meet & Multiply!")
   end
 end
