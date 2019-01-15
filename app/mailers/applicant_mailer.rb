e251b045904379eda9fdaf57ce1659791c9da6e2
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-01-15 17:07:09 -0500

    update email templates and add originator URLs

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -3,12 +3,14 @@ class ApplicantMailer < ApplicationMailer
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::UrlHelper
 
-  ORIGINATOR_PAGE_LOOKUP = {
-    "Originator 1" => "https://www.meetandmultiply.org/",
-    "Originator 2" => "https://www.meetandmultiply.org/",
-    "Originator 3" => "https://www.meetandmultiply.org/",
-    "Originator 4" => "https://www.meetandmultiply.org/",
+  ORIGINATOR_URL_LOOKUP = {
+    "Blooom" => "https://www.meetandmultiply.org/blooom",
+    "LifeBank" => "https://www.meetandmultiply.org/lifebank",
+    "ONergy" => "https://www.meetandmultiply.org/onergy",
+    "Ignitia" => "https://www.meetandmultiply.org/ignitia",
   }
+
+  EVENT_AGENDA_URL = "https://www.meetandmultiply.org/#contact"
 
   def confirm_email(applicant)
     @applicant = applicant
@@ -18,14 +20,15 @@ class ApplicantMailer < ApplicationMailer
   def acceptance_email(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
-    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
+    @originator_page_url = ORIGINATOR_URL_LOOKUP[@originator]
+    @agenda_url = EVENT_AGENDA_URL
     mail(to: @applicant.email, subject: "You’ve been accepted to attend Meet & Multiply")
   end
 
   def more_info(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
-    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
+    @originator_page_url = ORIGINATOR_URL_LOOKUP[@originator]
     mail(to: @applicant.email, subject: "More information needed for your Meet & Multiply application")
   end
 
@@ -37,7 +40,7 @@ class ApplicantMailer < ApplicationMailer
   def reminder(applicant)
     @applicant = applicant
     @originator = @applicant.bmodel
-    @originator_page_url = ORIGINATOR_PAGE_LOOKUP[@originator]
+    @originator_page_url = ORIGINATOR_URL_LOOKUP[@originator]
     mail(to: @applicant.email, subject: "Please confirm your attendance at Meet & Multiply!")
   end
 end
