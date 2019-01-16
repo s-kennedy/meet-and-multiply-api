4750b76a78585cde2a4409d24873220206f7ab77
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-01-16 12:39:44 -0500

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
    :port                 => 587,
    :user_name            => ENV['endeva_username'],
    :password             => ENV['endeva_password'],
-   :domain               => "endeva.org"
+   :domain               => "endeva.org",
    :authentication       => :login,
    :enable_starttls_auto => true
   }
