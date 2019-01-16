4d30a1838c5e1c3f3eb27a4919da09cda451e4d3
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-01-16 01:28:47 -0500

    add actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address              => "mail.endeva.org",
+   :port                 => 587,
+   :user_name            => ENV['endeva_username'],
+   :password             => ENV['endeva_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => 'www.meetandmultiply.org' }
 end
